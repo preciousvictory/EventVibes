6fe2908c3d4ca0ee38771b1bd0ee1f80763eea82
Abiodun-Omoniyi Victory <104084524+preciousvictory@users.noreply.github.com>
2025-03-30 00:57:25 +0100

    vercell

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/", // Ensures proper routing
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    port: 3000, // Optional: Set the local dev server port 
-  },
-  preview: {
-    port: 5000, // Optional: Set the preview server port
-  }
 });
